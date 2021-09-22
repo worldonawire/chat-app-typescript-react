cdd270ca348b5b7e5dd313d8f6e96b5bce766df6
Mark <markc.herring@gmail.com>
2021-09-22 18:17:55 +0900

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import GlobalStyle from "./styles/global.styles";
 import {
 	ApolloClient,
 	InMemoryCache,
-    HttpLink,
-    from,
-    ApolloProvider
+	HttpLink,
+	from,
+	ApolloProvider,
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 import { ErrorProvider } from "./contexts/ErrorContext";
@@ -23,45 +23,45 @@ const httpLink = new HttpLink({
 });
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-      ),
-    );
+	if (graphQLErrors)
+		graphQLErrors.forEach(({ message, locations, path }) =>
+			console.log(
+				`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+			)
+		);
 
 	if (networkError) {
 		console.log(`[Network error]: ${networkError}`);
 
-		if (typeof window !== 'undefined' && !window.navigator.onLine) {
-			alert('Sorry, your browser is offline.');
-		  } else {
-			alert('Some other network error occurred.');
-		  }	
+		if (typeof window !== "undefined" && !window.navigator.onLine) {
+			alert("Sorry, your browser is offline.");
+		} else {
+			alert("Some other network error occurred.");
+		}
 	}
 });
 
 const client = new ApolloClient({
 	link: from([errorLink, httpLink]),
-    cache: new InMemoryCache()
+	cache: new InMemoryCache(),
 });
 
 function App() {
 	return (
 		<ApolloProvider client={client}>
 			<UserProvider>
-        <ChannelProvider>
-							<SentProvider>
-          <TextProvider>
-						<ErrorProvider>
-            			<GlobalStyle />
-							<Container>
-								<Navbar />
-								<ChatBoard />	
-							</Container>
-					</ErrorProvider>
-          </TextProvider>
-							</SentProvider>
+				<ChannelProvider>
+					<SentProvider>
+						<TextProvider>
+							<ErrorProvider>
+								<GlobalStyle />
+								<Container>
+									<Navbar />
+									<ChatBoard />
+								</Container>
+							</ErrorProvider>
+						</TextProvider>
+					</SentProvider>
 				</ChannelProvider>
 			</UserProvider>
 		</ApolloProvider>
--- src/components/ChatBoard.tsx/index.tsx
+++ src/components/ChatBoard.tsx/index.tsx
@@ -1,14 +1,14 @@
 import React, { MouseEvent, useState, useEffect, useRef } from "react";
 import {
-    OuterContainer,
-    ChatRow,
-    ChatContainer,
-    ChatElement,
-    ChatText,
-    PersonIcon,
-    ImageContainer,
-    FetchButton,
-    Information,
+	OuterContainer,
+	ChatRow,
+	ChatContainer,
+	ChatElement,
+	ChatText,
+	PersonIcon,
+	ImageContainer,
+	FetchButton,
+	Information,
 } from "./styles";
 
 import PostMessage from "../PostMessage";
@@ -16,10 +16,10 @@ import PostMessage from "../PostMessage";
 import { gql, useQuery, useLazyQuery } from "@apollo/client";
 import { useChannelState } from "../../contexts/ChannelContext";
 import {
-    FaArrowDown,
-    FaArrowUp,
-    FaCheckCircle,
-    FaExclamationCircle,
+	FaArrowDown,
+	FaArrowUp,
+	FaCheckCircle,
+	FaExclamationCircle,
 } from "react-icons/fa";
 import { Col } from "../../App.styles";
 import { useErrorDispatch, useErrorState } from "../../contexts/ErrorContext";
@@ -52,284 +52,251 @@ const FETCH_MORE_MESSAGES = gql`
 `;
 
 interface MessageObject {
-    messageId: string;
-    text: string;
-    datetime?: string;
-    userId: string;
+	messageId: string;
+	text: string;
+	datetime?: string;
+	userId: string;
 }
 
 interface ErrorMessageObject {
-    messageId: string | undefined;
-    text: string | undefined;
-    userId: string | undefined;
-    timeAtError?: string | undefined;
+	messageId: string | undefined;
+	text: string | undefined;
+	userId: string | undefined;
+	timeAtError?: string | undefined;
 }
 
 export default function ChatBoard() {
-    let channelState = useChannelState();
-    let errorState = useErrorState();
-    let errorDispatch = useErrorDispatch();
-    let sentState = useSentState()
+	let channelState = useChannelState();
+	let errorState = useErrorState();
+	let errorDispatch = useErrorDispatch();
+	let sentState = useSentState();
 
-    const [state, setState] = useState<MessageObject[] | ErrorMessageObject[]>(
-        []
-    );
-    // let errorChat = useRef<string | boolean>(false);
-    useEffect(() => {
-        if (errorState.errMessage.timeAtError !== undefined) {
-            // errorChat.current = "something";
-            // errorDispatch({
-            //             type: "ERROR_MESSAGE",
-            //             payload: {
-            //                 ...errorState.errMessage,
-            //                 messageId: "",
-            //                 text: "",
-            //                 userId: "",
-            //                 timeAtError: undefined,
-            //             },
-            //         });
-            setState([errorState.errMessage, ...state]);
-            // return function cleanUp() {
-            //     errorDispatch({
-            //         type: "ERROR_MESSAGE",
-            //         payload: {
-            //             ...errorState.errMessage,
-            //             messageId: "",
-            //             text: "",
-            //             userId: "",
-            //             timeAtError: undefined,
-            //         },
-            //     });
-            // };
-        }
-    }, [errorState]);
+	const [state, setState] = useState<MessageObject[] | ErrorMessageObject[]>(
+		[]
+	);
 
-    const persistBool = useRef<string | boolean>("This should be a bool");
+	useEffect(() => {
+		if (errorState.errMessage.timeAtError !== undefined) {
+			setState([errorState.errMessage, ...state]);
+		}
+	}, [errorState]);
 
-    /* -------------------Fetch initial messages--------------------- */
-    const { loading, data, error, refetch } = useQuery(LATEST_MESSAGES, {
-        variables: { channelId: channelState.channel.channelID },
-        
-        errorPolicy: "all",
-    });
-    let emptyChat = useRef(true)
-    // let emptyChat: boolean = false;
-    let stateLoading: boolean = false;
+	const persistBool = useRef<string | boolean>("This should be a bool");
 
-    let timeAtRender = useRef(Date.now())
-    let firstAndLast = useRef<MessageObject[]>([]);
-    useEffect(() => {
-        if (data) {
-            refetch()
-            firstAndLast.current[0] = data.fetchLatestMessages[0];
-            firstAndLast.current[1] = data.fetchLatestMessages[data.fetchLatestMessages.length-1];
-            setState(data.fetchLatestMessages)    
-        } 
-    }, [channelState.channel.timeAtClick]);
+	/* -------------------Fetch initial messages--------------------- */
+	const { loading, data, error, refetch } = useQuery(LATEST_MESSAGES, {
+		variables: { channelId: channelState.channel.channelID },
 
-  
-    useEffect(() => {
-        if (!data || loading) {
-            stateLoading = true;
-        } else if (data.fetchLatestMessages.length === 0) {
-            emptyChat.current = true;
-            setState([])
-        } else if (data.fetchLatestMessages.length > 0) {
-            firstAndLast.current[0] = data.fetchLatestMessages[0];
-            firstAndLast.current[1] = data.fetchLatestMessages[data.fetchLatestMessages.length-1];
-            emptyChat.current = false;
-            setState(data.fetchLatestMessages);
-        }
-        console.log("Messages: ", state);
-    }, [data]);
+		errorPolicy: "all",
+	});
+	let emptyChat = useRef(true);
+	let stateLoading: boolean = false;
+	let firstAndLast = useRef<MessageObject[]>([]);
+	useEffect(() => {
+		if (data) {
+			refetch();
+			firstAndLast.current[0] = data.fetchLatestMessages[0];
+			firstAndLast.current[1] =
+				data.fetchLatestMessages[data.fetchLatestMessages.length - 1];
+			setState(data.fetchLatestMessages);
+		}
+	}, [channelState.channel.timeAtClick]);
 
-    let errorMessage: string | undefined;
-    if (
-        error?.networkError &&
-        typeof window !== "undefined" &&
-        !window.navigator.onLine
-    ) {
-        errorMessage = "Sorry your browser is offline.";
-    } else if (error?.graphQLErrors) {
-        errorMessage = error?.message;
-    }
+	useEffect(() => {
+		if (!data || loading) {
+			stateLoading = true;
+		} else if (data.fetchLatestMessages.length === 0) {
+			emptyChat.current = true;
+			setState([]);
+		} else if (data.fetchLatestMessages.length > 0) {
+			firstAndLast.current[0] = data.fetchLatestMessages[0];
+			firstAndLast.current[1] =
+				data.fetchLatestMessages[data.fetchLatestMessages.length - 1];
+			emptyChat.current = false;
+			setState(data.fetchLatestMessages);
+		}
+		console.log("Messages: ", state);
+	}, [data]);
 
-    /* -------------------Fetch more messages--------------------- */
-    const [
-        fetchMoreMessages,
-        { loading: fetchMoreLoading, data: fetchMoreData, error: fetchMoreErrors },
-    ] = useLazyQuery(FETCH_MORE_MESSAGES, { fetchPolicy: "network-only" });
+	let errorMessage: string | undefined;
+	if (
+		error?.networkError &&
+		typeof window !== "undefined" &&
+		!window.navigator.onLine
+	) {
+		errorMessage = "Sorry your browser is offline.";
+	} else if (error?.graphQLErrors) {
+		errorMessage = error?.message;
+	}
 
-    // let bool: boolean | undefined;
+	/* -------------------Fetch more messages--------------------- */
+	const [
+		fetchMoreMessages,
+		{ loading: fetchMoreLoading, data: fetchMoreData, error: fetchMoreErrors },
+	] = useLazyQuery(FETCH_MORE_MESSAGES, { fetchPolicy: "network-only" });
 
-    let newLoading: boolean = false;
-    let noMoreMessages = useRef(false)
-    // let noMoreMessages: boolean = false;
-    useEffect(() => {
-        if (!fetchMoreData || fetchMoreLoading) {
-            newLoading = true;
-        } else if (fetchMoreData && fetchMoreData.fetchMoreMessages.length === 0) {
-            noMoreMessages.current = true;
-        } else if (
-            fetchMoreData.fetchMoreMessages.length > 0 &&
-            persistBool.current === "true"
-        ) {
-            firstAndLast.current[0] = fetchMoreData.fetchMoreMessages[0];
-            firstAndLast.current[1] = fetchMoreData.fetchMoreMessages[fetchMoreData.fetchMoreMessages.length-1];
-            // noMoreMessages.current = false;
-            setState([...state, ...fetchMoreData.fetchMoreMessages]);
-        } else if (
-            fetchMoreData.fetchMoreMessages.length > 0 &&
-            persistBool.current === "false"
-        ) {
-            firstAndLast.current[0] = fetchMoreData.fetchMoreMessages[0];
-            firstAndLast.current[1] = fetchMoreData.fetchMoreMessages[fetchMoreData.fetchMoreMessages.length-1];
-            // noMoreMessages.current = true;
-            var reversedArray = [...fetchMoreData.fetchMoreMessages].reverse();
-            setState([...reversedArray, ...state]);
-        }
-    }, [fetchMoreData]);
+	let newLoading: boolean = false;
+	let noMoreMessages = useRef(false);
+	useEffect(() => {
+		if (!fetchMoreData || fetchMoreLoading) {
+			newLoading = true;
+		} else if (fetchMoreData && fetchMoreData.fetchMoreMessages.length === 0) {
+			noMoreMessages.current = true;
+		} else if (
+			fetchMoreData.fetchMoreMessages.length > 0 &&
+			persistBool.current === "true"
+		) {
+			firstAndLast.current[0] = fetchMoreData.fetchMoreMessages[0];
+			firstAndLast.current[1] =
+				fetchMoreData.fetchMoreMessages[
+					fetchMoreData.fetchMoreMessages.length - 1
+				];
+			setState([...state, ...fetchMoreData.fetchMoreMessages]);
+		} else if (
+			fetchMoreData.fetchMoreMessages.length > 0 &&
+			persistBool.current === "false"
+		) {
+			firstAndLast.current[0] = fetchMoreData.fetchMoreMessages[0];
+			firstAndLast.current[1] =
+				fetchMoreData.fetchMoreMessages[
+					fetchMoreData.fetchMoreMessages.length - 1
+				];
+			var reversedArray = [...fetchMoreData.fetchMoreMessages].reverse();
+			setState([...reversedArray, ...state]);
+		}
+	}, [fetchMoreData]);
 
-    const fetchHandler = (e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        let mssgId;
-        let bool;
-        persistBool.current = e.currentTarget.value;
-        if (e.currentTarget.value === "true" ) {
+	const fetchHandler = (e: MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		let mssgId;
+		let bool;
+		persistBool.current = e.currentTarget.value;
+		if (e.currentTarget.value === "true") {
 			bool = true;
 			mssgId = firstAndLast.current[1].messageId;
 		} else {
 			bool = false;
 			mssgId = firstAndLast.current[0].messageId;
 		}
-     
-        fetchMoreMessages({
-            variables: {
-                channelId: channelState.channel.channelID,
-                messageId: mssgId,
-                old: bool,
-            },
-        });
-        console.log("bool: ", bool);
-        console.log("After fetchMore messages lazy request");
-    };
 
-    return (
-        <Col
-            size={3}
-            color="#F4F5FB"
-            direction="column"
-            height="100vh"
-            paddingLeft="1em"
-            paddingRight="1em"
-            paddingBottom="1em"
-        >
-            <OuterContainer>
-                <h1
-                    style={{
-                        color: "#495057",
-                        borderBottom: "1px solid #e6ecf3",
-                        marginBottom: "0.5em",
-                        padding: "0.5em 0",
-                    }}
-                >
-                    {channelState.channel.name}
-                </h1>
-               
-                    <FetchButton value="true" onClick={fetchHandler}>
-                        Read More
-                        <FaArrowUp />
-                    </FetchButton>
-               
-                <ChatRow>
-                    {loading || fetchMoreLoading ? (
-                        <p>Loading...</p>
-                    ) : error ? (
-                        <p>There was an error loading the page</p>
-                    ) : null}
-                    {emptyChat.current ? (
-                        <p>This chat is empty, please send a message!</p>
-                    ) : state ? (
-                        state.map((user: any) => (
-                            <div key={user.messageId}>
-                                <ChatContainer
-                                    userId={user.userId}
-                                    latestUser={state[0].userId}
-                                >
-                                    <ChatElement
-                                        userId={user.userId}
-                                        latestUser={state[0].userId}
-                                    >
-                                        <ImageContainer>
-                                            <PersonIcon userId={user.userId} />
-                                            <p
-                                                style={{
-                                                    marginTop: "-1em",
-                                                    padding: "0px",
-                                                    verticalAlign: "top",
-                                                }}
-                                            >
-                                                {user.userId}
-                                            </p>
-                                        </ImageContainer>
-                                        <ChatText>
-                                            <span style={{ position: "relative", top: "-1em" }}>
-                                                {user.text}
-                                            </span>
-                                        </ChatText>
-                                        {user.userId === state[0].userId ? (
-                                            <div
-                                                style={{
-                                                    flex: "display",
-                                                    flexDirection: "row-reverse",
-                                                }}
-                                            >
-                                                <Information
-                                                    userId={user.userId}
-                                                    latestUser={state[0].userId}
-                                                >
-                                                    <p style={{ margin: "0 0.25em" }}>
-                                                        {user.datetime
-                                                            ? user.datetime.substr(11, 5)
-                                                            : user.timeAtError.substr(11, 5)}
-                                                    </p>
-                                                    {user.datetime ? (
-                                                        <FaCheckCircle style={{ color: "#9EC94A" }} />
-                                                    ) : (
-                                                        <FaExclamationCircle style={{ color: "#B71E3C" }} />
-                                                    )}
-                                                    {user.datetime ? (
-                                                        <span style={{ margin: "0 0.25em" }}>sent</span>
-                                                    ) : (
-                                                        <span style={{ margin: "0 0.25em" }}>unsent</span>
-                                                    )}
-                                                </Information>
-                                            </div>
-                                        ) : (
-                                            <p style={{ margin: "0 0.25em" }}>
-                                                {user.datetime.substr(11, 5)}
-                                            </p>
-                                        )}
-                                    </ChatElement>
-                                </ChatContainer>
-                            </div>
-                        ))
-                    ) : null}
-                </ChatRow>
-                {/* {errorChat ? <div>Hey</div> : null} */}
-               
-                    <FetchButton value="false" onClick={fetchHandler}>
-                        Read More
-                        <FaArrowDown />
-                    </FetchButton>
-            
-                <PostMessage />
-            </OuterContainer>
-        </Col>
-    );
+		fetchMoreMessages({
+			variables: {
+				channelId: channelState.channel.channelID,
+				messageId: mssgId,
+				old: bool,
+			},
+		});
+		console.log("bool: ", bool);
+		console.log("After fetchMore messages lazy request");
+	};
+
+	return (
+		<Col
+			size={3}
+			color="#F4F5FB"
+			direction="column"
+			height="100vh"
+			paddingLeft="1em"
+			paddingRight="1em"
+			paddingBottom="1em"
+		>
+			<OuterContainer>
+				<h1
+					style={{
+						color: "#495057",
+						borderBottom: "1px solid #e6ecf3",
+						marginBottom: "0.5em",
+						padding: "0.5em 0",
+					}}
+				>
+					{channelState.channel.name}
+				</h1>
+				<FetchButton value="true" onClick={fetchHandler}>
+					Read More
+					<FaArrowUp />
+				</FetchButton>
+				<ChatRow>
+					{loading || fetchMoreLoading ? (
+						<p>Loading...</p>
+					) : error ? (
+						<p>There was an error loading the page</p>
+					) : null}
+					{emptyChat.current ? (
+						<p>This chat is empty, please send a message!</p>
+					) : state ? (
+						state.map((user: any) => (
+							<div key={user.messageId}>
+								<ChatContainer
+									userId={user.userId}
+									latestUser={state[0].userId}
+								>
+									<ChatElement
+										userId={user.userId}
+										latestUser={state[0].userId}
+									>
+										<ImageContainer>
+											<PersonIcon userId={user.userId} />
+											<p
+												style={{
+													marginTop: "-1em",
+													padding: "0px",
+													verticalAlign: "top",
+												}}
+											>
+												{user.userId}
+											</p>
+										</ImageContainer>
+										<ChatText>
+											<span style={{ position: "relative", top: "-1em" }}>
+												{user.text}
+											</span>
+										</ChatText>
+										{user.userId === state[0].userId ? (
+											<div
+												style={{
+													flex: "display",
+													flexDirection: "row-reverse",
+												}}
+											>
+												<Information
+													userId={user.userId}
+													latestUser={state[0].userId}
+												>
+													<p style={{ margin: "0 0.25em" }}>
+														{user.datetime
+															? user.datetime.substr(11, 5)
+															: user.timeAtError.substr(11, 5)}
+													</p>
+													{user.datetime ? (
+														<FaCheckCircle style={{ color: "#9EC94A" }} />
+													) : (
+														<FaExclamationCircle style={{ color: "#B71E3C" }} />
+													)}
+													{user.datetime ? (
+														<span style={{ margin: "0 0.25em" }}>sent</span>
+													) : (
+														<span style={{ margin: "0 0.25em" }}>unsent</span>
+													)}
+												</Information>
+											</div>
+										) : (
+											<p style={{ margin: "0 0.25em" }}>
+												{user.datetime.substr(11, 5)}
+											</p>
+										)}
+									</ChatElement>
+								</ChatContainer>
+							</div>
+						))
+					) : null}
+				</ChatRow>
+				<FetchButton value="false" onClick={fetchHandler}>
+					Read More
+					<FaArrowDown />
+				</FetchButton>
+
+				<PostMessage />
+			</OuterContainer>
+		</Col>
+	);
 }
-
-
-// !noMoreMessages.current ? (
-//     <p>No further messages</p>
-// ) : 
